ef51e947e42058d428132add5d117bb83dc2a845
Mohammad Ghorab <mody.moustafa@yahoo.com>
2025-06-13 20:21:56 +0200

    Refactor Onboarding component: update styles for buttons and layout, modify onboarding messages for clarity, and streamline the structure of the PageOnboarding function.

--- storefront/app/components/Onboarding.tsx
+++ storefront/app/components/Onboarding.tsx
@@ -6,11 +6,10 @@
  */
 
 import Link from "next/link";
-import { useIsPresentationTool } from "next-sanity/hooks";
 import { createDataAttribute } from "next-sanity";
-import { uuid } from "@sanity/uuid";
-
 import { studioUrl } from "@/sanity/lib/api";
+import { useIsPresentationTool } from "next-sanity/hooks";
+import { uuid } from "@sanity/uuid";
 
 type OnboardingMessageProps = {
   message: {
@@ -44,7 +43,7 @@ const OnboardingMessage = ({
       <div>
         {!isPresentation ? (
           <Link
-            className="inline-flex rounded-full gap-2 items-center bg-white text-red-500 hover:bg-red-100 focus:bg-red-200 py-3 px-6 transition-colors duration-200"
+            className="inline-flex rounded-full gap-2 items-center bg-neutral-950 text-white hover:bg-neutral-800 focus:bg-neutral-700 py-3 px-6 transition-colors duration-200"
             href={link.href}
             target="_blank"
           >
@@ -62,7 +61,7 @@ const OnboardingMessage = ({
           </Link>
         ) : (
           <button
-            className="cursor-pointer inline-flex rounded-full gap-2 items-center bg-white text-red-500 hover:bg-red-100 focus:bg-red-200 py-3 px-6 transition-colors duration-200"
+            className="cursor-pointer inline-flex rounded-full gap-2 items-center bg-neutral-950 text-white hover:bg-neutral-800 focus:bg-neutral-700 py-3 px-6 transition-colors duration-200"
             data-sanity={createDataAttribute({
               id: uuid(),
               type,
@@ -87,86 +86,18 @@ const OnboardingMessage = ({
   );
 };
 
-export default function Onboarding() {
-  return (
-    <div className="max-w-2xl mx-auto grid grid-flow-row gap-6 py-12 text-center bg-red-500 text-white rounded-lg p-8">
-      <svg
-        className="mx-auto h-10 w-10 text-gray-400"
-        aria-hidden="true"
-        width="512"
-        height="512"
-        viewBox="0 0 512 512"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <rect width="512" height="512" fill="#ffffff" rx="30" />
-        <path
-          d="M161.527 136.723C161.527 179.76 187.738 205.443 240.388 219.095L296 232.283C345.687 243.852 376 272.775 376 319.514C376 341.727 369.162 360.931 357.538 375.971C357.538 329.232 333.607 303.78 276.171 288.74L221.47 276.246C177.709 266.065 143.977 242.464 143.977 191.56C143.977 170.505 150.359 151.994 161.527 136.723Z"
-          fill="#F03E2F"
-        />
-        <path
-          opacity="0.5"
-          d="M323.35 308.176C347.054 323.679 357.538 345.197 357.538 376.202C337.709 401.654 303.293 416 262.724 416C194.575 416 146.484 381.756 136 322.753H201.641C210.074 350.056 232.41 362.551 262.268 362.551C298.735 362.32 322.895 342.652 323.35 308.176Z"
-          fill="#F03E2F"
-        />
-        <path
-          opacity="0.5"
-          d="M195.715 200.816C172.923 186.007 161.527 165.183 161.527 136.954C180.672 111.503 213.493 96 253.835 96C323.35 96 363.692 133.252 373.721 185.776H310.359C303.293 165.183 285.971 148.986 254.291 148.986C220.33 148.986 197.311 169.116 195.715 200.816Z"
-          fill="#F03E2F"
-        />
-      </svg>
-      <OnboardingMessage
-        message={{
-          title: "No posts yet",
-          description: "Get started by creating a new post.",
-        }}
-        link={{
-          title: "Create Post",
-          href: `${studioUrl}/structure/intent/create/template=post;type=post;path=title`,
-        }}
-        type="post"
-        path="title"
-      />
-    </div>
-  );
-}
-
 export function PageOnboarding() {
   return (
-    <div className="max-w-2xl mx-auto grid grid-flow-row gap-6 py-12 text-center bg-red-500 text-white rounded-lg p-8">
-      <svg
-        className="mx-auto h-10 w-10 text-gray-400"
-        aria-hidden="true"
-        width="512"
-        height="512"
-        viewBox="0 0 512 512"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <rect width="512" height="512" fill="#ffffff" rx="30" />
-        <path
-          d="M161.527 136.723C161.527 179.76 187.738 205.443 240.388 219.095L296 232.283C345.687 243.852 376 272.775 376 319.514C376 341.727 369.162 360.931 357.538 375.971C357.538 329.232 333.607 303.78 276.171 288.74L221.47 276.246C177.709 266.065 143.977 242.464 143.977 191.56C143.977 170.505 150.359 151.994 161.527 136.723Z"
-          fill="#F03E2F"
-        />
-        <path
-          opacity="0.5"
-          d="M323.35 308.176C347.054 323.679 357.538 345.197 357.538 376.202C337.709 401.654 303.293 416 262.724 416C194.575 416 146.484 381.756 136 322.753H201.641C210.074 350.056 232.41 362.551 262.268 362.551C298.735 362.32 322.895 342.652 323.35 308.176Z"
-          fill="#F03E2F"
-        />
-        <path
-          opacity="0.5"
-          d="M195.715 200.816C172.923 186.007 161.527 165.183 161.527 136.954C180.672 111.503 213.493 96 253.835 96C323.35 96 363.692 133.252 373.721 185.776H310.359C303.293 165.183 285.971 148.986 254.291 148.986C220.33 148.986 197.311 169.116 195.715 200.816Z"
-          fill="#F03E2F"
-        />
-      </svg>
+    <div className="max-w-2xl mx-auto grid grid-flow-row gap-6 py-12 text-center bg-white text-neutral-950 rounded-lg p-8">
       <OnboardingMessage
         message={{
-          title: "About Page (/about) does not exist yet",
-          description: "Get started by creating an about page.",
+          title: "This page has no content!",
+          description: "Open the page in Sanity Studio to add content.",
         }}
         link={{
-          title: "Create Page",
-          href: `${studioUrl}/structure/intent/create/template=page;type=page;path=name`,
+          title: "Add content to this page",
+          // href: `${studioUrl}/structure/intent/edit/template=page;type=page;path=pageBuilder;id=$`,
+          href: ``,
         }}
         type="page"
         path="name"
