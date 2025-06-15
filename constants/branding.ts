5a8e6566e158f2ca4a969de60f37eb8b24f01e41
Mohammad Ghorab <mody.moustafa@yahoo.com>
2025-06-15 17:30:49 +0200

    Update branding constants: change project name and description to reflect new focus on National Capital, emphasizing its diverse business platforms and market presence in Saudi Arabia.

--- constants/branding.ts
+++ constants/branding.ts
@@ -1,4 +1,5 @@
-export const NAME = 'Clean Next.js + Sanity'
-export const DESCRIPTION = 'A clean Next.js + Sanity starter template'
-export const OG_IMAGE_TITLE = 'A clean Next.js + Sanity starter template'
+export const NAME = "National Capital";
+export const DESCRIPTION =
+  "National Capital has multiple platforms active in different fields of businesses, which varies from project development, to investment, business solutions services and trading. Building on years of active participation and successful execution of numerous projects, combined with a vast network and deep influence in the Kingdom of Saudi Arabia, National Capital has positioned itself as a one of the key players capable of tapping into the large and rapidly growing Saudi market both as developer, investor and service provider.";
+export const OG_IMAGE_TITLE = "National Capital";
 // export const LOGO = '/logo.svg' // TODO: Add logo
